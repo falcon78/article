6ccdbfd61d10abcd82e38fac37186d8dec5a09b8
falcon78 <khatri.sujan.infodex@gmail.com>
2019-05-24 11:23:41 +0900

    fixed markdown parsing issue

--- src/Components/Content/supporters/components/molecules/MarkdownConverterToHtml.js
+++ src/Components/Content/supporters/components/molecules/MarkdownConverterToHtml.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 function MarkdownConverterToHtml({ markdown }) {
   const output = markdown.replace(/\\n/g, '  \n'); // Markdownは”\n”の前スペースを二回開けないと改行されません。
-  const outputFinal = output.replace(/#/g, '# ');
+  const outputFinal = output.replace(/#{,1}/g, '# ');
 
   return (
     <FontStyle>
